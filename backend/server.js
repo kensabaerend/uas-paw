c100b3a7e3c116e016ec84d5ba7582232ff7d3fd
kensabaerend <95273087+kensabaerend@users.noreply.github.com>
2023-12-23 20:52:29 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,3 +29,7 @@ require("./app/routes/etalase.route")(app);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`server started on port http://localhost:${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send("Welcome to my API...");
+});
